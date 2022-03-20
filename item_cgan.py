0316c535c171abadfb126f487c9ad72af7b4c22e
Your Name <you@example.com>
2022-03-20 17:41:24 -0300

    OK v1

--- item_cgan.py
+++ item_cgan.py
@@ -19,16 +19,17 @@ from torch.utils.data import Dataset, DataLoader
 import torch
 
 os.makedirs("images", exist_ok=True)
+os.makedirs("results/images", exist_ok=True)
+os.makedirs("results/models", exist_ok=True)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
+parser.add_argument("--n_epochs", type=int, default=2000, help="number of epochs of training")
 parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
 parser.add_argument("--latent_dim", type=int, default=30, help="dimensionality of the latent space")
-parser.add_argument("--n_classes", type=int, default=10, help="number of classes for dataset")
 parser.add_argument("--img_size", type=int, default=64, help="size of each image dimension")
 parser.add_argument("--channels", type=int, default=1, help="number of image channels")
 parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
@@ -149,7 +150,6 @@ LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
 
 
 def sample_image(n_row, batches_done):
-    """Saves a grid of generated digits ranging from 0 to n_classes"""
     # Sample noise
     z = FloatTensor(np.random.normal(0, 1, (n_row ** 2, opt.latent_dim)))
 
